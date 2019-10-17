5d12047caa919a1ef0cff60148afb7e55f5f3ede
PatrizioC <patrizio.colomba@gmail.com>
2019-10-17 12:35:52 +0200

    Adds two new functions

--- strvect.c
+++ strvect.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include "strvect.h"
 
-#define MIN_SIZE 4
-#define DIM_BUFFER 1024
+#define MIN_SIZE 	4
+#define BUFFER_SIZE 1024
 
 void print_strvect(char **vector, int n) {
 	for(int i = 0; i < n; i++) {
-		printf("%s\n", vector[i]);
+		printf("%s", vector[i]);
+		char* newline = strchr(vector[i], '\n');
+		if(newline) {
+			int len = strlen(vector[i]);
+			if(newline != &(vector[i][len-1])) puts("");
+		} else puts("");
 	}
 }
 
 char** file_row_list(FILE *f, char **vector, int *n) {
 	int size = MIN_SIZE;
 	vector = calloc(size, sizeof(vector));
-	char str[DIM_BUFFER];
+	char str[BUFFER_SIZE];
 	(*n) = 0;
-	while(fgets(str, DIM_BUFFER, f)) {
+	while(fgets(str, BUFFER_SIZE, f)) {
 		vector[(*n)++] = strdup(str);
 		if((*n) >= size) {
 			size += MIN_SIZE;
@@ -28,3 +34,26 @@ char** file_row_list(FILE *f, char **vector, int *n) {
 	
 	return vector;
 }
+
+void free_strvect(char** vector, int n) {
+	for(int i = 0; i < n; i++) {
+		free(vector[i]);
+	}
+	free(vector);
+}
+
+
+char** strvect(int n, ...) {
+	char **vector = calloc(n, sizeof(vector));
+	va_list args;
+    va_start(args, n);
+
+	for(int i = 0; i < n; i++) {
+		vector[i] = strdup(va_arg(args, char *));
+	}
+
+	va_end(args);
+
+	return vector;
+}
+
--- strvect.h
+++ strvect.h
@@ -3,7 +3,8 @@
 
 void print_strvect(char **vector, int n);
 char** file_row_list(FILE *f, char **vettore, int *n);
+void free_strvect(char** vector, int n);
+char** strvect(int n, ...);
 
 #endif
-
 
